9036e33c29919ac059fdfb6222217e3ba410743f
Oscar Garcia <oscar.garcia.og3@roche.com>
2019-09-26 17:11:44 +0200

    init

--- src/sagas/rootSaga.js
+++ src/sagas/rootSaga.js
@@ -2,67 +2,43 @@ import * as effects from "redux-saga/effects";
 import * as constants from "../actions/constants";
 
 function* processTaskTimeConsuming() {
-  const delay = 1000 + 1000 * Math.random();
-  console.log("delay time", delay);
-  yield effects.delay(delay);
-  if (Math.random() > 0.5) {
-    return true;
-  } else {
-    console.log("processerror");
-    throw new Error("Something is not right!");
-  }
+  yield effects.delay(1000 + 1000 * Math.random());
 }
 
-function processTaskTimeConsumingReal() {
-  return fetch("https://swapi.co/api/people/1/")
-    .then(response => response)
-    .catch(e => {
-      throw new Error("error in request");
-    });
-}
-
-function* doProcessTask(action) {
-  const actionName = action.name;
-  console.log("action", actionName);
-  yield effects.put({ type: constants.TASK_PROCESS_START, name: actionName });
-  console.log("action dispatcehd");
-  let result;
+function* doProcessTask(name) {
   try {
-    result = yield effects.call(processTaskTimeConsuming);
-    console.log("request finished", result);
-    yield effects.put({
-      type: constants.TASK_PROCESS_DONE,
-      name: actionName
-    });
+    yield effects.put({ type: constants.TASK_PROCESS_START, name });
+    yield effects.call(processTaskTimeConsuming);
+    yield effects.put({ type: constants.TASK_PROCESS_DONE, name });
   } catch (error) {
-    console.log("error", error);
-    yield effects.put({
-      type: constants.TASK_PROCESS_ERROR,
-      name: actionName
-    });
+    yield effects.put({ type: constants.TASK_PROCESS_ERROR, name });
+  } finally {
+    if (yield effects.cancelled()) {
+      yield effects.put({ type: constants.TASK_PROCESS_RESET, name });
+    }
   }
 }
+
+function* processTask(action) {
+  yield effects.call(doProcessTask, action.name);
+}
+
 function* processTaskTimed(action) {
-  console.log("processTaskTimed", action);
-  const res = yield effects.race({
-    response: effects.call(doProcessTask, action),
-    cancelled: effects.take(constants.TASK_PROCESS_CANCEL_ALL),
-    timeout: effects.delay(1500)
+  yield effects.race({
+    response: effects.call(doProcessTask, action.name),
+    cancelled: effects.take(constants.TASK_PROCESS_CANCEL_ALL)
   });
-  if (res.cancelled) {
-    yield effects.put({
-      type: constants.TASK_PROCESS_RESET,
-      name: action.name
-    });
-  }
-  if (res.timeout) {
-    yield effects.put({
-      type: constants.TASK_PROCESS_ERROR,
-      name: action.name
-    });
+}
+function* processAll() {
+  const tasks = yield effects.select(state =>
+    state.tasks.filter(task => task.status === constants.statuses.IDLE)
+  );
+  for (let task of tasks) {
+    yield effects.fork(doProcessTask, task.name);
   }
 }
+
 export function* rootSaga() {
-  console.log("hi from sagas");
   yield effects.takeEvery(constants.TASK_PROCESS, processTaskTimed);
+  yield effects.takeEvery(constants.TASK_PROCESS_ALL, processAll);
 }
