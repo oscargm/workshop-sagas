d545f84a9f770e7dea74e87d19a57b9bfb1ca307
Oscar Garcia <oscar.garcia.og3@roche.com>
2019-09-26 17:52:36 +0200

    added tests

--- src/sagas/rootSaga.test.js
+++ src/sagas/rootSaga.test.js
@@ -1,4 +1,5 @@
 import { rootSaga } from "./rootSaga";
+import { expectSaga } from "redux-saga-test-plan";
 import * as effects from "redux-saga/effects";
 import * as constants from "../actions/constants";
 import { processTask } from "./rootSaga";
@@ -8,4 +9,69 @@ test("it should fork processTask and wsHandler", () => {
   expect(it.next().value).toEqual(
     effects.takeEvery(constants.TASK_PROCESS, processTask)
   );
+});
+
+test("handles TASK_PROCESS!", () => {
+  return expectSaga(rootSaga)
+    .put({
+      type: constants.TASK_PROCESS_START,
+      name: "a name"
+    })
+
+    .dispatch({
+      type: constants.TASK_PROCESS,
+      name: "a name"
+    })
+    .run();
+});
+
+test("handles TASK_PROCESS!", () => {
+  return expectSaga(rootSaga)
+    .provide([[effects.call(processTaskTimeConsuming), "mock return value"]])
+    .put({
+      type: constants.TASK_PROCESS_START,
+      name: "a name"
+    })
+
+    .dispatch({
+      type: constants.TASK_PROCESS,
+      name: "a name"
+    })
+    .run();
+});
+
+test("handles TASK_PROCESS!", () => {
+  return expectSaga(rootSaga)
+    .provide([[effects.call(processTaskTimeConsuming)]])
+    .put({
+      type: constants.TASK_PROCESS_START,
+      name: "a name"
+    })
+    .put({
+      type: constants.TASK_PROCESS_ERROR,
+      name: "a name"
+    })
+    .dispatch({
+      type: constants.TASK_DONE,
+      name: "a name"
+    })
+    .run();
+});
+
+test("handles TASK_PROCESS!", () => {
+  return expectSaga(rootSaga)
+    .provide([[effects.call(processTaskTimeConsuming), throwError("an error")]])
+    .put({
+      type: constants.TASK_PROCESS_START,
+      name: "a name"
+    })
+    .put({
+      type: constants.TASK_PROCESS_ERROR,
+      name: "a name"
+    })
+    .dispatch({
+      type: constants.TASK_PROCESS,
+      name: "a name"
+    })
+    .run();
 });
